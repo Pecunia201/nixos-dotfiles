1b3364322211b9caafab5391dbd734537672691f
Laurie <laurie201@protonmail.ch>
2024-09-22 12:03:30 +0100

    Initial modularisation

---
+++ hm_modules/zsh.nix
@@ -0,0 +1,52 @@
+{ config, ... }:
+
+{
+  programs.zsh = {
+    enable = true;
+    autocd = true;
+    dotDir = ".config/zsh";
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+    enableCompletion = true;
+
+    zplug = {
+      enable = true;
+      plugins = [
+        {
+          name = "romkatv/powerlevel10k";
+          tags = [as:theme depth:1];
+        }
+      ];
+    };
+
+    shellAliases =
+      let
+        flakeDir = "~/nixos";
+      in {
+        ls = "ls --color='always'";
+        v = "vim";
+        rebuild = "sudo nixos-rebuild switch --flake ${flakeDir}#default";
+        config = "sudo vim ${flakeDir}/configuration.nix";
+        hm = "sudo vim ${flakeDir}/home.nix";
+        ff = "fastfetch";
+    };
+
+    # p10k Home manager config: https://github.com/nix-community/home-manager/issues/1338#issuecomment-651807792
+    initExtraBeforeCompInit = ''
+      # p10k instant prompt
+      local P10K_INSTANT_PROMPT="${config.xdg.cacheHome}/p10k-instant-prompt-''${(%):-%n}.zsh"
+      [[ ! -r "$P10K_INSTANT_PROMPT" ]] || source "$P10K_INSTANT_PROMPT"
+    '';
+
+    initExtra = ''
+      # Powerlevel10k config
+      typeset -g POWERLEVEL9K_SHORTEN_STRATEGY=
+      typeset -g POWERLEVEL9K_SHORTEN_DIR_LENGTH=2
+      typeset -g POWERLEVEL9K_INSTANT_PROMPT=quiet
+
+      if [[ -f $XDG_DATA_HOME/zsh/zshrc ]]; then source $XDG_DATA_HOME/zsh/zshrc; fi
+
+      source $HOME/nixos/configs/p10k
+    '';
+  };
+}
--- home.nix
+++ home.nix
@@ -11,6 +11,10 @@
     allowUnfree = true;
   };
 
+  imports = [
+    ./hm_modules/zsh.nix
+  ];
+  
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
   # introduces backwards incompatible changes.
@@ -35,54 +39,6 @@
 
   programs.alacritty = {
 	enable = true;
-  };
-  
-  programs.zsh = {
-    enable = true;
-    autocd = true;
-    dotDir = ".config/zsh";
-    autosuggestion.enable = true;
-    syntaxHighlighting.enable = true;
-    enableCompletion = true;
-
-    zplug = {
-      enable = true;
-      plugins = [
-        {
-          name = "romkatv/powerlevel10k";
-          tags = [as:theme depth:1];
-        }
-      ];
-    };
-    
-    shellAliases =
-      let
-	flakeDir = "~/nixos";
-      in {
-        ls = "ls --color='always'";
-        v = "vim";
-        rebuild = "sudo nixos-rebuild switch --flake ${flakeDir}#default";
-        config = "sudo vim ${flakeDir}/configuration.nix";
-        hm = "sudo vim ${flakeDir}/home.nix";
-    };
-
-    # p10k Home manager config: https://github.com/nix-community/home-manager/issues/1338#issuecomment-651807792
-    initExtraBeforeCompInit = ''
-      # p10k instant prompt
-      local P10K_INSTANT_PROMPT="${config.xdg.cacheHome}/p10k-instant-prompt-''${(%):-%n}.zsh"
-      [[ ! -r "$P10K_INSTANT_PROMPT" ]] || source "$P10K_INSTANT_PROMPT"
-    '';
-
-    initExtra = ''
-      # Powerlevel10k config
-      typeset -g POWERLEVEL9K_SHORTEN_STRATEGY=
-      typeset -g POWERLEVEL9K_SHORTEN_DIR_LENGTH=2
-      typeset -g POWERLEVEL9K_INSTANT_PROMPT=quiet
-
-      if [[ -f $XDG_DATA_HOME/zsh/zshrc ]]; then source $XDG_DATA_HOME/zsh/zshrc; fi
-      
-      source $HOME/nixos/configs/p10k
-    '';
   };
   
   # configure virt-manager
