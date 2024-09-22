79c07a53b1f53cc8455526a8e00d7f011548c6f2
Laurie <laurie201@protonmail.ch>
2024-09-22 12:12:12 +0100

    Modularised zsh

--- home.nix
+++ home.nix
@@ -2,8 +2,6 @@
 { config, pkgs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
   home.username = "pecunia";
   home.homeDirectory = "/home/pecunia";
 
@@ -15,14 +13,8 @@
     ./hm_modules/zsh.nix
   ];
   
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "24.05"; # Please read the comment before changing.
+  # Home manager
+  home.stateVersion = "24.05";
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
@@ -58,10 +50,7 @@
 	};
   };
   
-  programs.bash = {
-	enable = true;
-  };
-  
+  # for dev environments
   programs.direnv = {
 	enable = true;
 	enableBashIntegration = true;
